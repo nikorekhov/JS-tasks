7f976aab57f20694b1cda5c980b35669f880de8d
nikorekhov <orekhovnikolay@mail.ru>
2017-11-20 03:18:11 +0300

    js

---
+++ BOM/bom.js
@@ -0,0 +1,21 @@
+function addBlock(number){
+    for(var i = 0; i < number; i++){
+        $('.block').append('<li>'+ (i+1) + '</li>');
+    } 
+}
+function changeColor(color){
+    $('li').css('bakcground-color', color);
+}
+var color = $('#color').attr('value');
+var number = $('#number').attr('value');
+$(document).ready(function(){    
+    $('#color').on('change',function(){
+        color = $('#color').val();
+        //$('ul').css('bakcground-color', color);
+        changeColor(color);
+    });
+    $('#number').on('change',function(){
+        number = $('#number').val();
+        addBlock(number,color);
+    });
+});
